f68b23a7a2935e4eeddc54a0e1df76efb285f5b8
Alexander <akurmazov2001@gmail.com>
2020-10-04 17:36:29 +0400

    Add tests for accounts module

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from knox import views as knox_views
 from accounts.api import RegisterAPI, LoginAPI
 
 urlpatterns = [
-    path('api/accounts/register', RegisterAPI.as_view()),
-    path('api/accounts/login', LoginAPI.as_view()),
-    path('api/accounts/logout', knox_views.LogoutView.as_view())
+    path('api/accounts/register', RegisterAPI.as_view(), name='register'),
+    path('api/accounts/login', LoginAPI.as_view(), name='login'),
+    path('api/accounts/logout', knox_views.LogoutView.as_view(), name='logout')
 ]
