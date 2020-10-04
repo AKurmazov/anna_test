c4376b6ddd7dd0388c98d83a8ea755b259991d1f
Alexander <akurmazov2001@gmail.com>
2020-10-04 22:22:51 +0400

    Alter task retrieve-update endpoint url with no enclosing slash

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,6 @@ from tasks.api import TaskListAPI, TaskCreateAPI, TaskRetrieveUpdateAPI, ChangeL
 urlpatterns = [
     path('api/tasks/all', TaskListAPI.as_view(), name='tasks-all'),
     path('api/tasks/create', TaskCreateAPI.as_view(), name='task-create'),
-    path('api/tasks/<pk>/', TaskRetrieveUpdateAPI.as_view(), name='task-retrieve-update'),
+    path('api/tasks/<pk>', TaskRetrieveUpdateAPI.as_view(), name='task-retrieve-update'),
     path('api/tasks/<pk>/changes', ChangeLogListAPI.as_view(), name='task-changes')
 ]
